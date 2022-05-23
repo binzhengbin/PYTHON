180d36dd4924edf56a8132d074f8139f0610234b
binzhengbin <87288334+binzhengbin@users.noreply.github.com>
2022-05-23 09:43:40 +0800

    Initial commit

---
+++ 2022-05-23.py
@@ -0,0 +1,114 @@
+### 第五章
+
+codon_table = {
+    'GCU':'A', 'GCC':'A', 'GCA':'A', 'GCG':'A', 'CGU':'R', 'CGC':'R',
+    'CGA':'R', 'CGG':'R', 'AGA':'R', 'AGG':'R', 'UCU':'S', 'UCC':'S',
+    'UCA':'S', 'UCG':'S', 'AGU':'S', 'AGC':'S', 'AUU':'I', 'AUC':'I',
+    'AUA':'I', 'UUA':'L', 'UUG':'L', 'CUU':'L', 'CUC':'L', 'CUA':'L',
+    'CUG':'L', 'GGU':'G', 'GGC':'G', 'GGA':'G', 'GGG':'G', 'GUU':'V',
+    'GUC':'V', 'GUA':'V', 'GUG':'V', 'ACU':'T', 'ACC':'T', 'ACA':'T',
+    'ACG':'T', 'CCU':'P', 'CCC':'P', 'CCA':'P', 'CCG':'P', 'AAU':'N',
+    'AAC':'N', 'GAU':'D', 'GAC':'D', 'UGU':'C', 'UGC':'C', 'CAA':'Q',
+    'CAG':'Q', 'GAA':'E', 'GAG':'E', 'CAU':'H', 'CAC':'H', 'AAA':'K',
+    'AAG':'K', 'UUU':'F', 'UUC':'F', 'UAU':'Y', 'UAC':'Y', 'AUG':'M',
+    'UGG':'W',
+    'UAG':'STOP', 'UGA':'STOP', 'UAA':'STOP'
+    }
+
+# read the RNA sequence into a single string
+rna = ''
+for line in open('A06662-RNA.fasta'):
+    if not line.startswith('>'):
+        rna = rna + line.strip()
+
+# translate one frame at a time
+for frame in range(3):
+    prot = ''
+    print('Reading frame ' + str(frame + 1))
+    for i in range(frame, len(rna), 3):
+        codon = rna[i:i + 3]
+        if codon in codon_table:
+            if codon_table[codon] == 'STOP':
+                prot = prot + '*'
+            else:
+                prot = prot + codon_table[codon]
+        else:
+            # handle too short codons
+            prot = prot + '-'
+
+    # format to blocks of 48 columns
+    i = 0
+    while i < len(prot):
+        print(prot[i:i + 48])
+        i = i + 48
+
+
+#  字典
+# 真（TRUE）假（FALSE）布尔值
+n = 0
+while n < 4:
+    n = n + 1
+    print(n)
+
+# 字典为'键（key）：值（value）'对的对象的无序集合，用花括号括起来：{'GCU':'A', 'GCC':'A'}
+# 更特别的是，字典是不可变对象（键）映射任意对象（值）的结构。不可变对象可以是数字、字符串和元组。这意味着，，列表和字典本身不能用作字典键，但可以用作值。
+print(codon_table['GCU'])  # 字典可用于快速搜信息。
+
+codon_table2 = {'GCU': 'A'}
+print(codon_table2)
+codon_table2['CGA'] = 'R'
+print(codon_table2)
+
+print(codon_table2['CGA'])
+
+del codon_table2['CGA']  # 删除 del
+print(codon_table2)
+
+if 'GCU' in codon_table2:
+    print(1)
+else:
+    print(0)
+
+print(len(codon_table2))  # 计算字典所含有元素的数量
+
+
+### while 语句
+# 为了生成更易阅读的结果，蛋白质序列按每行 48 个符号的块来进行输出
+# i = 0
+# while i < len(prot):
+#    print(prot[i:i + 48])
+#    i = i + 48
+# while 语句会重复执行语句块直到一些条件得道满足。while循环实际上是for和if语句的组合。
+
+# while <condition>:
+#       <statements>
+# while 1: # 1 对应的条件是TRUE， 0是FALSE
+#     print('while loop still running')
+
+swissprot = open('SwissProt.fasta')
+insulin_ac = 'P61981'
+result = None
+while result is None:
+    line = swissprot.__next__()
+    if line.startswith('>'):
+        ac = line.split('|')[1]
+        if ac == insulin_ac:
+            result = line.strip()
+            print(result)
+
+# 如果while语句找不到胰岛素登记码，程序除非插入一个break语句，否则就要在文件到达末尾是通过StopIteration报错结束。
+# break语句
+# 当解释器遇到 break 语句时会退出循环，不再执行循环语句的其余部分，包括 else语句(如果存在），直接进到循环后面的第一条语句
+# continue语句
+# continue语句跳过循环语句的其余部分，跳至下 次循环
+
+
+# 列表搜索
+bases = ['A', 'C', 'T', 'G']
+seq = 'CAGGCCATTRKGL'
+for i in seq:
+    if i not in bases:
+        print(i, 'is not a nucleotide')
+
+
+
