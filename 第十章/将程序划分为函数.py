5bb6864ae545459010ab5fedd076b87e282b055f
binzhengbin <87288334+binzhengbin@users.noreply.github.com>
2022-06-01 23:45:47 +0800

    20220531

--- 第十章/将程序划分为函数.py
+++ 第十章/将程序划分为函数.py
@@ -74,4 +74,47 @@ print(g(2))
 print((lambda x: x**3)(3))
 
 # 在python中有四类参数：必选参数、关键字参数、默认参数和可变长参数
+# 必选参数
+def print_funct(num, seq):
+    print(num, seq)
+print_funct(10, 'ACCTGGCACAA')
+# 关键字参数
+def print_funct(num, seq):
+    print(num, seq)
+print_funct(seq = 'ACCTGGCACAA', num=10)
+# 默认参数(可选参数)，这些可选参数必须放在函数定义的最后位置，默认参数应该为不可变类型（数字、字符串），不能使用列表或字典作为默认参数。
+def print_funct(num, seq = 'A'):
+    print(num, seq)
+print_funct(10, 'ACCTGGCACAA')
+print_funct(10)
+# 可变长参数
+def print_args(*args): # 一个*返回元组
+    print(args)
+print_args(1, 2, 3, 4, 5)
+print('hello, world!')
+print(100, 200, 'ACCTGGCACAA')
+def print_args2(**args): # 两个*返回字典
+    print(args)
+print_args2(num = 100, num2 = 200, seq = 'ACCTGGCACAA')
+
+# struct模块
+# 准确地讲，Python没有专门处理字节的数据类型。但由于b'str'可以表示字节，所以，字节数组＝二进制str。
+import struct
+format = '2s1s1s1s1s'
+a = struct.pack(format, b'10', b'2', b'3', b'4', b'5')
+print(a)
+print(a.decode('utf-8'))
+
+# unpack() parses the string to a tuple
+# In Python 3, struct() require bytes
+format = '1s2s1s1s'
+line = '12345'
+col = struct.unpack(format, line.encode('utf-8'))
+col_to_string = [line.decode('utf-8') for line in col]
+print(col)
+print(col_to_string)
+
+format = '30s30s20s1s'
+b = struct.calcsize(format)  # 返回给定格式化字符串的字符总数。
+print(b)
 
